c409612835ca66e0fe9e21d9fa427a625a1a5dbd
kevinisback <banyuyang@hotmail.com>
2022-11-27 10:44:26 +1100

    you were so wrong lady

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Cindy, I will prover you so wrong.
+          Edit <code>src/App.js</code> and save to reload. Cindy, I will prover you were so wrong.
         </p>
         <a
           className="App-link"
